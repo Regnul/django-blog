9014594d80a50a4a8c0ead8797e512d4a3f51b1a
Regnal <peelb1@gmail.com>
2022-05-25 15:05:03 -0700

    Completed data model

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from blogging.models import Post
+from blogging.models import Post, Category
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Category)
--- blogging/tests.py
+++ blogging/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from blogging.models import Post
+from blogging.models import Post, Category
 
 class PostTestCase(TestCase):
     fixtures = ['blogging_test_fixture.json', ]
@@ -14,3 +14,10 @@ class PostTestCase(TestCase):
         actual = str(p1)
         self.assertEqual(expected, actual)
     
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
