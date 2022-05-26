799ea27fd47e8ddae1ba887f80b39b6b99a5c485
Regnal <peelb1@gmail.com>
2022-05-25 19:20:15 -0700

    detailed view

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,3 +1,5 @@
+import datetime 
+from django.utils.timezone import utc
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post, Category
@@ -21,3 +23,41 @@ class CategoryTestCase(TestCase):
         c1 = Category(name=expected)
         actual = str(c1)
         self.assertEqual(expected, actual)
+
+class FrontEndTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count, 
+                    text="foo",
+                    author=author)
+            if count < 6:
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue("Recent Posts" in resp_text)
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            if count < 6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
+
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
--- blogging/views.py
+++ blogging/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
 
 # Create your views here.
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
