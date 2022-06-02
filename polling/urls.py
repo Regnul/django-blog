20187f888895574e459ef9ab66f512c2032ec1c0
Regnal <peelb1@gmail.com>
2022-06-01 23:03:57 -0700

    polling model class upgrade

--- polling/urls.py
+++ polling/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from polling.views import list_view, detail_view
+from polling.views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
-    ]
+    path('', PollListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>/', PollDetailView, name="poll_detail"),
+    ] 
--- polling/views.py
+++ polling/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def list_view(request):
-    context = {'polls': Poll.objects.all()}
-    return render(request, 'polling/list.html', context)
 
-def detail_view(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+class PollListView(ListView):
+    model = Poll
+    template_name = 'polling/list.html'
 
-    if request.method == "POST":
+class PollDetailView(DetailView):
+    model = Poll 
+    template_name = 'polling/detail.html'
+
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+
         if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
 
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+        context = {'object': poll}
+        return render(request, 'polling/detail.html', context)
