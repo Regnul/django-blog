f5129744395b5d2256da0ab233163b019aa61cde
Regnal <peelb1@gmail.com>
2022-06-08 14:01:05 -0700

    Fixed issues

--- blogging/urls.py
+++ blogging/urls.py
@@ -3,5 +3,5 @@ from blogging.views import BloggingListView, BloggingDetailView
 
 urlpatterns = [
         path('', BloggingListView.as_view(), name="blog_index"),
-        path('posts/<int:pk>/', BloggingDetailView, name="blog_detail"),
+        path('posts/<int:pk>/', BloggingDetailView.as_view(), name="blog_detail"),
         ] 
