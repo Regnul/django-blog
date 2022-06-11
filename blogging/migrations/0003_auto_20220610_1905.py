4f9c41e367d978957fd0bbefc4d09786211fc23a
Regnal <peelb1@gmail.com>
2022-06-10 19:12:56 -0700

    same as before, but pass black

--- blogging/migrations/0003_auto_20220610_1905.py
+++ blogging/migrations/0003_auto_20220610_1905.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blogging', '0002_category'),
+        ("blogging", "0002_category"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='category',
-            options={'verbose_name_plural': 'Categories'},
+            name="category",
+            options={"verbose_name_plural": "Categories"},
         ),
     ]
